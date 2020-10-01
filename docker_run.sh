dcb5b20b98d1012348d8c3061ff05be1805ac09a
martin nievas <martin.nievas.ar@gmail.com>
2020-10-01 17:03:13 -0300

    Add script for run applications inside Docker container

---
+++ docker_run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+application=$1
+shift
+options="$@"
+
+case "$application" in
+  openambit)          builddir=opeanmbit/openambit;;
+  ambitconsole)       builddir=example/ambitconsole;;
+  openambit-cli)      builddir=openambit-cli/openambit-cli;;
+  *)
+    echo "$application: not supported" >&2
+    exit 1
+    ;;
+esac
+
+docker run --rm -v /home/snow/repos/openambit:/code --device=/dev/hidraw4 openambit:stretch /code/_build/src/${builddir} ${options}
